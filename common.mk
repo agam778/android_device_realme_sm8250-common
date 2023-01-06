ddc1920b345f0bbcd9163ec565f07bd4e381c982
Blackmanx <blackman@sajl.tk>
2023-01-06 22:31:33 +0100

    sm8250-common: overlay: Add some overlay configs for yaap

--- common.mk
+++ common.mk
@@ -327,6 +327,10 @@ PRODUCT_PACKAGES += \
     OPlusTetheringResCommon \
     WifiResTarget
 
+# YAAP Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-yaap
+
 # Partition
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
