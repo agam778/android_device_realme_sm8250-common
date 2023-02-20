d03301bcef9f4fe41bfa6aa7e6b1afbbc2b33113
Blackmanx <blackman@sajl.tk>
2023-02-20 12:11:00 +0100

    sm8250-commmon: Revert back to qti power HAL

--- common.mk
+++ common.mk
@@ -353,7 +353,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.oplus-libperfmgr
+    android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
